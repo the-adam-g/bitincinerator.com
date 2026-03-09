16a674b3c33bdeebe8a6c0671ea779d30f3d89d3
Adam Gillion <adam@queuedesk.org>
2026-03-09 22:07:03 +0000

    First release of bitincinerator

---
+++ versions/v1.py
@@ -0,0 +1,23 @@
+import os
+import secrets
+from random import randint
+
+path = input("Enter the path of your file\n")
+path = path.strip('"')
+root, name = os.path.split(path)
+try:
+    newpath = root + "\\incinerated.txt"
+    os.rename(path, newpath)
+except:
+    newpath = root + "\\incinerated.txt"
+    os.rename(path, newpath)
+file = open(newpath, "rb")
+contentslength = len(file.read())
+print(contentslength)
+file.close()
+file = open(newpath, "wb")
+file.truncate()
+file.write(secrets.token_bytes(contentslength))
+file.close()
+os.remove(newpath)
+print("File has been successfully overwritten and deleted.")
